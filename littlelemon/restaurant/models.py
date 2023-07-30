f51bb42c4884481a48f3ced54bff288847b4adeb
adivats <21954913+adivats@users.noreply.github.com>
2023-07-29 21:12:33 -0700

    Add serializers and views

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class Menu(models.Model):
     title = models.CharField(max_length=255)
-    price = models.DecimalField(default=0, max_digits=10, decimal_places=2)
+    price = models.DecimalField(default=0, max_digits=10, decimal_places=2, validators = [MinValueValidator(0)])
     inventory = models.PositiveSmallIntegerField(default=0)
 
     def __str__(self)-> str:
